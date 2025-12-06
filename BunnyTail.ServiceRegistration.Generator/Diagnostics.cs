85b364829a12570a7f0ac6f1c53ba81e5b43ed40
usausa <machi.pon@gmail.com>
2025-12-06 12:21:47 +0900

    Update generator

--- BunnyTail.ServiceRegistration.Generator/Diagnostics.cs
+++ BunnyTail.ServiceRegistration.Generator/Diagnostics.cs
@@ -4,7 +4,7 @@ using Microsoft.CodeAnalysis;
 
 internal static class Diagnostics
 {
-    public static DiagnosticDescriptor InvalidMethodDefinition => new(
+    public static DiagnosticDescriptor InvalidMethodDefinition { get; } = new(
         id: "BTSR0001",
         title: "Invalid method definition",
         messageFormat: "Method must be partial extension. method=[{0}]",
@@ -12,7 +12,7 @@ internal static class Diagnostics
         defaultSeverity: DiagnosticSeverity.Warning,
         isEnabledByDefault: true);
 
-    public static DiagnosticDescriptor InvalidMethodParameter => new(
+    public static DiagnosticDescriptor InvalidMethodParameter { get; } = new(
         id: "BTSR0002",
         title: "Invalid method parameter",
         messageFormat: "Parameter type must be IServiceCollection. method=[{0}]",
@@ -20,7 +20,7 @@ internal static class Diagnostics
         defaultSeverity: DiagnosticSeverity.Warning,
         isEnabledByDefault: true);
 
-    public static DiagnosticDescriptor InvalidMethodReturnType => new(
+    public static DiagnosticDescriptor InvalidMethodReturnType { get; } = new(
         id: "BTSR0003",
         title: "Invalid method return type",
         messageFormat: "Return type must be IServiceCollection. method=[{0}]",
